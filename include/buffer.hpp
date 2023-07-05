b7b96790640ac19adadf281c7b86da80bc23dc81
Eessh <eesshkumarjagana@gmail.com>
2023-07-05 18:01:00 +0530

    implemented: text buffer

---
+++ include/buffer.hpp
@@ -0,0 +1,43 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include "types.hpp"
+
+class Buffer
+{
+public:
+  /// @brief Default constructor.
+  Buffer();
+
+  /// @brief Creates buffer with the given string.
+  /// @param init_string const reference to the string
+  ///        with which the buffer should be initialized.
+  Buffer(const std::string& init_string);
+
+  /// @brief Creates buffer with given lines.
+  /// @param lines const reference to vector of strings.
+  Buffer(const std::vector<std::string>& lines);
+
+  /// @brief Destructor
+  ~Buffer() = default;
+
+  [[nodiscard]] bool load_from_file(const std::string& filepath);
+
+  [[nodiscard]] const std::vector<std::string>& lines() const;
+
+  [[nodiscard]] std::pair<uint32, int32> cursor_coords() const;
+
+  [[nodiscard]] bool has_selection() const;
+
+  [[nodiscard]] const std::pair<std::pair<uint32, int32>,
+                                std::pair<uint32, int32>>&
+  selection() const;
+
+private:
+  uint32 _cursor_row;
+  int32 _cursor_col;
+  bool _has_selection;
+  std::pair<std::pair<uint32, int32>, std::pair<uint32, int32>> _selection;
+  std::vector<std::string> _lines;
+};
---
+++ src/buffer.cpp
@@ -0,0 +1,56 @@
+#include "../include/buffer.hpp"
+#include "../include/macros.hpp"
+
+Buffer::Buffer()
+  : _cursor_row(0)
+  , _cursor_col(-1)
+  , _has_selection(false)
+  , _selection({{0, -1}, {0, -1}})
+  , _lines({""})
+{}
+
+Buffer::Buffer(const std::string& init_string)
+  : _cursor_row(0)
+  , _cursor_col(-1)
+  , _has_selection(false)
+  , _selection({{0, -1}, {0, -1}})
+  , _lines({init_string})
+{}
+
+Buffer::Buffer(const std::vector<std::string>& lines)
+  : _cursor_row(0)
+  , _cursor_col(-1)
+  , _has_selection(false)
+  , _selection({{0, -1}, {0, -1}})
+  , _lines(lines)
+{}
+
+bool Buffer::load_from_file(const std::string& filepath)
+{
+  // TODO
+}
+
+const std::vector<std::string>& Buffer::lines() const
+{
+  return _lines;
+}
+
+std::pair<uint32, int32> Buffer::cursor_coords() const
+{
+  return std::make_pair(_cursor_row, _cursor_col);
+}
+
+bool Buffer::has_selection() const
+{
+  return _has_selection;
+}
+
+const std::pair<std::pair<uint32, int32>, std::pair<uint32, int32>>&
+Buffer::selection() const
+{
+  if(!_has_selection)
+  {
+    ERROR_BOII("Called GET selection, when buffer has no selection!");
+  }
+  return _selection;
+}
